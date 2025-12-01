149ff6b9fff4a3a51c30b78190d6b6cdac6c89e1
Marzouk <nemendmd@gmail.com>
2025-12-01 14:39:13 +0100

    optimisation CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Indique à Tailwind où chercher les classes utilisées pour générer le CSS
-  content: [
-    "./**/*.{html,js}", 
-    "./pages/**/*.html",
-    "./script/**/*.js"
-  ],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: "#ef4444",
         dark: "#020617",
-        // J'ai harmonisé darkSoft sur la version catalogue (#0f0f0f) qui est plus logique pour une nuance
-        darkSoft: "#0f0f0f", 
+        darkSoft: "#0f0f0f",
       },
       fontFamily: {
         sans: ["Inter", "system-ui", "sans-serif"],
